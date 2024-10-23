9bd30c6c5188a3a67e753a740eb6160364fc96d4
raphaelagil <raphaela.gil@yahoo.com>
2024-10-23 18:58:17 +0100

    removed the margin from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ import Logo from "../assets/logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 rounded-lg shadow m-4">
+    <footer className="bg-gray-900 rounded-lg shadow">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">          
-          <img src={Logo} className="h-44" alt="Flowbite Logo" />
+          <img src={Logo} className="h-32 md:h-44" alt="Flowbite Logo" />
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className="hover:underline hover:text-amber-200 me-4 md:me-6">Sobre</a>
